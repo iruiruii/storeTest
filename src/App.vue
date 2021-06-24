c35c2ad2e740eeed93494de6056626f563161b1f
iruiuii <xwt1016@126.com>
2021-06-24 16:05:14 +0800

    基础样式

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,14 @@
 
 import Index from './components/Index.vue'
 
-export default {
-  name: 'App',
-  components: {
-    Index
+  export default {
+    name: 'App',
+    components: {
+      Index
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './assets/css/base.css';
 </style>
